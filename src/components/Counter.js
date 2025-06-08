69048c66d4879dbd6d71548a0a85854fbfcb7110
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-08 23:28:09 +0300

    L-311. Access store in Counter component to get and display store counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,15 @@
+import { useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter); // by using useSelector, react automatically subscribes this component to the store
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
